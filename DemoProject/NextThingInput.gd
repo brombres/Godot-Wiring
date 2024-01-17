a834f18bbfc863ac50ddfc17572cbf573ba93bc5
Brom Bresenham <brom.bresenham@runegate.com>
2024-01-17 10:35:16 -0500

    [README]

--- DemoProject/NextThingInput.gd
+++ DemoProject/NextThingInput.gd
@@ -11,6 +11,6 @@ func add_thing():
 		if event_log.text: event_log.text += "\n"
 		event_log.text += "Added " + text
 
-		var v_scroll = Wiring.TextEdit.get_v_scroll_bar()
+		var v_scroll = event_log.get_v_scroll_bar()
 		v_scroll.value = v_scroll.max_value
 		text = ""
--- addons/Wiring/Wiring.gd
+++ addons/Wiring/Wiring.gd
@@ -1,29 +1,40 @@
 extends Node
 
-var _active_scene:Node
-var _node_cache := {}
+var _cached_scene:Node
+var _cache := {}
+
+## Clears the node cache. Note: this happens automatically when the scene changes.
+func clear_cache():
+	_cache = {}
 
 func _get( node_name:StringName )->Variant:
-	var cur_scene = get_tree().current_scene
-	if not cur_scene:
-		push_warning( "[Wiring] No current scene when accessing Wiring.%s" % [node_name] )
-		return null
+	var cur_scene = _get_current_scene( node_name )
+	if not cur_scene: return null
 
-	if _active_scene != cur_scene:
-		_active_scene = cur_scene
-		_node_cache = {}
+	if _cache.has(node_name):
+		return _cache[node_name]
 
-	if _node_cache.has(node_name):
-		return _node_cache[node_name]
-
-	var node = _active_scene.find_child( node_name )
+	var node = _cached_scene.find_child( node_name )
 	if node:
-		_node_cache[node_name] = node
+		_cache[node_name] = node
 		return node
 
 	push_error( "[Wiring] No such node in current scene: %s" % [node_name] )
 	return null
 
+func _get_current_scene( node_name:StringName )->Variant:
+	var cur_scene = get_tree().current_scene
+	if not cur_scene:
+		push_warning( "[Wiring] No current scene when accessing Wiring.%s" % [node_name] )
+		return null
+
+	if _cached_scene != cur_scene:
+		_cached_scene = cur_scene
+		_cache = {}
+
+	return cur_scene
+
 func _set( node_name:StringName, value:Variant ):
-	_node_cache[ node_name ] = value
+	_get_current_scene( node_name )  # clears the cache if necessary
+	_cache[ node_name ] = value
 	return true
